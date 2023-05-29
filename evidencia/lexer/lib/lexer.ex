5402487126d2b02c33788d9da0af0cb0d75e1577
Shaul Zayat <shaulzayat1@gmail.com>
2023-05-29 15:28:05 -0600

    Create Elixir sigil

--- evidencia/lexer/lib/lexer.ex
+++ evidencia/lexer/lib/lexer.ex
@@ -3,16 +3,40 @@ defmodule Lexer do
   Documentation for `Lexer`.
   """
 
-  @doc """
-  Hello world.
+  def marker(in_filename, out_filename, offset) do
+    data = in_filname
+    |> File.stream!()
+    |> Stream.map(&marker_line(&1, offset))
+    |> Enum.join("")
 
-  ## Examples
+    File.write(out_filename, data)
 
-      iex> Lexer.hello()
-      :world
 
-  """
-  def hello do
-    :world
   end
+
+  defp marker_line(string) do
+    comment = ~r|^(#[^\n]+)|
+    variable = ~r|^([a-zA-Z_]\w*)|
+    boolean = ~r|^(True|False)|
+    number = ~r|^[+-]?\d+(\.\d+)?|
+    string = ~r|^("[^"]*"|'[^']*')|
+    arithmetic_op = ~r|^((\*\*)|(\/\/)|\+|\-|\*|\/|\%)|
+    assignment_op = ~r|^((\=)|(\*\*\=)|(\/\/\=)|(\+\=)|(\-\=)|(\*\=)|(\/\=)|(\%\=))|
+    comparison_op = ~r|^((\=\=)|(\!\=)|(\>\=)|(\<\=)|(\>)|(\<))|
+    logical_op = ~r<^(and|or|not)>
+    membership_op = ~r|^(in|not in)|
+    identity_op = ~r|^(is|is not)|
+    bitwise_op = ~r<^(\||\&|\^|\~|(\>\>)|(\<\<))>
+    whitespace = ~r|^(\s+)|
+    endline = ~r|^(\n)|
+    tab = ~r|^(\t)|
+    parenthesis = ~r|^(\(|\))|
+    bracket = ~r|^(\[|\])|
+    curly_bracket = ~r|^(\{|\})|
+    comma = ~r|^(\,)|
+    colon = ~r|^(\:)|
+    keyword = ~r|^(def|del|None|as|assert|break|class|continue|for|from|global|if|elif|else|except|finally|import|lambda|nonlocal|pass|raise|return|try|while|with|yield)|
+    method = ~r|^(input|print|range|len|str|int|float|bool|list|type)|
+  end
+
 end
